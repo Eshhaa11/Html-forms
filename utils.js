7dcf54c63712521e0b88ced6ff1dbb11207cbb87
Eshhaa11 <patellesha11@gmail.com>
2025-02-12 12:55:39 +0300

    added

--- utils.js
+++ utils.js
@@ -28,7 +28,10 @@ function validatePhoneNumber(phoneNumber){
 }
 
 function validateEmail (email){
-    
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email)
+
+
 }
 
 
