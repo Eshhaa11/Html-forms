df4d2235d72a02b15731cf27be029e5824e6b687
Eshhaa11 <patellesha11@gmail.com>
2025-02-12 21:52:12 +0300

    add

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ const submitBtn = document.getElementById("submitBtn")
 const alertSpan = document.getElementById("alert-password")
 
 firstName.addEventListener("change" , (e) => {
-    const name = e.target.value
-    details.firstName =  name
+    const firstName = e.target.value
+    details.firstName =  firstName
 })
 lastName.addEventListener("change" , (e) => {
-    const name = e.target.value
-    details.lastName = e.target.value
+    const lastname = e.target.value
+    details.lastName = lastName
 })
 email.addEventListener("change" , (e) => {
     const email = e.target.value
-    details.email = e.target.value
+    details.email = email
 })
 phoneNumber.addEventListener("change", (e) => {
     const phone = e.target.value;
@@ -44,37 +44,59 @@ phoneNumber.addEventListener("change", (e) => {
 
 password.addEventListener("change" , (e) => {
     const password = e.target.value
-    details.password = e.target.value
+    details.password = password
 })
 confirmPassword.addEventListener("change" , (e) => {
     const confirmPassword = e.target.value
-    details.confirmPassword = e.target.value
+    details.confirmPassword = confirmPassword
 
 
 })
-
 function validatePassword(password, confirmPassword) {
-    if (password === confirmPassword) {
-        alert("Passwords match.");
+    if (password === confirmPassword && password.length >= 4 && password !== "" && password.length !== 0 && password.trim() !== 0 && /\d/.test(password) && /[a-zA-Z]/.test(password))
+       {
+        // alert("Passwords match.");
         return true;
     }
 
     else {
-        alert("Passwords do not match. Please re-enter.");
+        // alert("Passwords do not match. Please re-enter.");
         return false;
     }
 }
 
+function validateNames(firstName, lastName){
+    if (firstName.length <= 4 && lastName.length <= 4 ){
+        return false
+    } else {
+        return true
+    }
+    //Using Tests,validate that the first name and last name are not less than 4 characters
+
+}
+
+function validatePhoneNumber(phoneNumber) {
+    const phoneRegex = /^(?:\+254|0|254)(7\d{8})$/;
+    return phoneRegex.test(phoneNumber);
+}
+
+function validateEmail (Email){
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(Email)
+}
+
 submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log(details)
-    let {firstName, lastName, email, phoneNumber, password, confirmPassword, agreeTerms} = details
-    console.log(firstName, lastName, email, phoneNumber, password, confirmPassword, agreeTerms)
+    const {firstName, lastName, email, phoneNumber, password, confirmPassword, agreeTerms} = details
 
+    
     const isValidated = validatePassword(password,confirmPassword)
-
-    if(isValidated) {
-        window.location.href = "/dashboard.html"
+    const isNameValidated = validateNames(firstName, lastName)
+    const isEmailValidated = validateEmail(email)
+    const isPhoneNumberValidated = validatePhoneNumber(phoneNumber)
+    console.log(isNameValidated, isEmailValidated, isPhoneNumberValidated,isValidated)
+    
+    if(isValidated && isNameValidated && isEmailValidated && isPhoneNumberValidated) {
+        window.location.href = "./dashboard.html"
 
     } else {
         alertSpan.classList.remove("alert-off-message")
